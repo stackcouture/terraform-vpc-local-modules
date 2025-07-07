95945bc565d831d62ce7adfc5ff923798638ad56
Naveen <naveenramlu28@gmail.com>
2025-07-07 12:34:04 +0530

    updated ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,18 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"]
+}
+
 resource "aws_instance" "ec2_b" {
-  ami                         = data.ubuntu.id
+  ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   subnet_id                   = values(var.public_subnet_ids)[0]
   key_name                    = "stackcouture-key"
